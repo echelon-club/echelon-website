49b4e330dcdc8633ef1d44028cb6b4daa17a6f1c
Ramachandran <Tarunsankar4@gmail.com>
2026-03-13 22:11:55 +0530

    Echelon Club

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ function Navbar(){
     return (
         <nav className="nav">
             <img src="echelon.jpeg" className="logo-mark"/>
-            <h3 className="nav-label">Echelon</h3>
+            <h3 className="nav-label">Echelon Club</h3>
         </nav>
     );
 }
